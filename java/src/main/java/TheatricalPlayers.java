0309d89ac38cbfe26479cff42237ef177038421a
Roland.Germ <roland.germ@nagarro.com>
2020-06-30 09:36:43 +0200

    migrated back to java8

--- java/src/main/java/TheatricalPlayers.java
+++ java/src/main/java/TheatricalPlayers.java
@@ -1,24 +1,23 @@
 import java.text.NumberFormat;
 import java.util.Locale;
-import java.util.Map;
 
 public class TheatricalPlayers {
 
     public String print(Invoice invoice) {
-        var totalAmount = 0;
-        var volumeCredits = 0;
-        var result = String.format("Statement for %s\n", invoice.customer);
+        double totalAmount = 0;
+        int volumeCredits = 0;
+        String result = String.format("Statement for %s\n", invoice.customer);
 
         NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (var perf : invoice.performances) {
-            var play = perf.play;
-            var thisAmount = 40000;
+        for (Performance perf : invoice.performances) {
+            Play play = perf.play;
+            double thisAmount = 40000;
             if (perf.audience > 30) {
                 thisAmount += 1000 * (perf.audience - 30);
             }
 
-            var thisCredits = Math.max(perf.audience - 30, 0);
+            int thisCredits = Math.max(perf.audience - 30, 0);
             if ("comedy".equals(play.type)) thisCredits += Math.floor((double) perf.audience / 5);
 
             totalAmount += thisAmount;
--- java/src/test/java/TheatricalPlayersTests.java
+++ java/src/test/java/TheatricalPlayersTests.java
@@ -1,23 +1,23 @@
+import static org.approvaltests.Approvals.verify;
+
+import java.util.Arrays;
+
 import org.approvaltests.reporters.JunitReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.Test;
-
-import java.util.List;
-
-import static org.approvaltests.Approvals.verify;
 
 @UseReporter(JunitReporter.class)
 public class TheatricalPlayersTests {
 
     @Test
     public void exampleStatement() {
-        Invoice invoice = new Invoice("BigCo", List.of(
+        Invoice invoice = new Invoice("BigCo", Arrays.asList(
                 new Performance(new Play("Hamlet", "tragedy"), 55),
                 new Performance(new Play("As You Like It", "comedy"), 35),
                 new Performance(new Play("Othello", "tragedy"), 40)));
 
         TheatricalPlayers theatricalPlayers = new TheatricalPlayers();
-        var result = theatricalPlayers.print(invoice);
+        String result = theatricalPlayers.print(invoice);
 
         verify(result);
     }
